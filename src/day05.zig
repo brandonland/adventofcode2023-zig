6972049fb8be87ef56a6dc324f2a98847df19856
Brandon <brandon@brandon.land>
2023-12-11 17:54:09 -0600

    solved day 6, cleanup on day 5.2

--- src/day05.zig
+++ src/day05.zig
@@ -51,39 +51,43 @@ fn isSourceMapped(input: u64, row: [3]u64) bool {
     return input >= row[1] and input <= row[1] + row[2];
 }
 
+fn getSeedsPart1(seeds_text: []const u8, allocator: Allocator) !List(u64) {
+    var result: List(u64) = List(u64).init(allocator);
+
+    var it = splitSca(u8, seeds_text, ' ');
+    while (it.next()) |seed_str| {
+        const seed_num = try parseInt(u64, seed_str, 10);
+        try result.append(seed_num);
+    }
+
+    return result;
+}
+
 fn part1() !u64 {
     var sections = splitSeq(u8, data, "\n\n");
     var seeds_line = splitSeq(u8, sections.next().?, ": ");
     _ = seeds_line.next();
     const seeds_text = seeds_line.next().?;
 
-    var seeds = splitSca(u8, seeds_text, ' ');
-    _ = &seeds;
-
-    var seeds_count: u8 = 0;
-    while (seeds.next()) |seed| {
-        _ = seed;
-        seeds_count += 1;
-    }
-    seeds.reset();
-
-    const chars_to_strip = "abcdeghijklmnopqrstuvwxyz-:";
-
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
+    //var seeds = splitSca(u8, seeds_text, ' ');
+    const seeds_list = try getSeedsPart1(seeds_text, allocator);
+
+    const chars_to_strip = "abcdeghijklmnopqrstuvwxyz-:";
+
     var location_nums: List(u64) = List(u64).init(allocator);
     var new_input: ?u64 = null;
 
-    while (seeds.next()) |seed| {
-        const initial_input = try parseInt(u64, seed, 10);
+    for (seeds_list.items) |seed_int| {
         new_input = null;
 
         var section_count: u8 = 1;
         while (sections.next()) |section| {
             var matched_row: ?[3]u64 = null;
-            const input = if (new_input == null) initial_input else new_input.?;
+            const input = if (new_input == null) seed_int else new_input.?;
 
             const text = trim(u8, section, chars_to_strip);
             var rows_text = splitSca(u8, text, '\n');
@@ -137,8 +141,143 @@ fn part1() !u64 {
     return std.mem.min(u64, location_nums.items);
 }
 
+const Chunk = enum { chunk1, chunk2 };
+
+fn getSeedsPart2(seeds_text: []const u8, chunk: Chunk, allocator: Allocator) !List(u64) {
+    var result: List(u64) = List(u64).init(allocator);
+    defer result.deinit();
+
+    var pass_num: u16 = 1;
+    var it = splitSca(u8, seeds_text, ' ');
+
+    //const interval = 1000;
+    //_ = interval;
+
+    while (it.next()) |seed_str| {
+        if (chunk == .chunk1) {
+            if (pass_num > 5) break;
+        }
+        if (chunk == .chunk2) {
+            print("Continuing...\n", .{});
+            if (pass_num <= 5) {
+                pass_num += 1;
+                continue;
+            }
+        }
+        const range_start = try parseInt(u64, seed_str, 10);
+        const end_range = try parseInt(u64, it.peek().?, 10);
+        var i: u64 = 0;
+        var num: u64 = range_start;
+
+        print("Start range: {d}, End range: {d}\n", .{ range_start, end_range });
+
+        while (i < end_range) : (i += 1) {
+            try result.append(num);
+            if (num == range_start + 100000) {
+                print("Pass number: {d}/10. Appending num: {d}\n", .{ pass_num, num });
+            }
+            num += 1;
+        }
+
+        pass_num += 1;
+        _ = it.next(); // The first seed and every odd-numbered seed is the "range_start" seed.
+    }
+
+    return result;
+}
+
 fn part2() !u64 {
-    return 0;
+    var sections = splitSeq(u8, data, "\n\n");
+    var seeds_line = splitSeq(u8, sections.next().?, ": ");
+    _ = seeds_line.next();
+
+    const seeds_text = seeds_line.next().?;
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const seeds_chunk1 = try getSeedsPart2(seeds_text, .chunk1, allocator);
+    //const seeds_chunk2 = try getSeedsPart2(seeds_text, .chunk2, allocator);
+    //print("Seeds:\n{any}", .{seeds});
+    //
+    print("Length of seeds_chunk1: {d}\n", .{seeds_chunk1.items.len});
+    //print("Length of seeds_chunk2: {d}\n", .{seeds_chunk2.items.len});
+
+    const chars_to_strip = "abcdeghijklmnopqrstuvwxyz-:";
+
+    //var location_nums: List(u64) = List(u64).init(allocator);
+    //_ = &location_nums;
+    var loc_num: ?u64 = null;
+    var new_input: ?u64 = null;
+
+    for (seeds_chunk1.items) |seed_int| {
+        new_input = null;
+
+        print("Planting seed {d}\n", .{seed_int});
+
+        var section_count: u8 = 1;
+        while (sections.next()) |section| {
+            var matched_row: ?[3]u64 = null;
+            const input = if (new_input == null) seed_int else new_input.?;
+
+            const text = trim(u8, section, chars_to_strip);
+            var rows_text = splitSca(u8, text, '\n');
+            _ = rows_text.next().?; // Skip title of section
+
+            var rows: List([3]u64) = List([3]u64).init(allocator);
+
+            while (rows_text.next()) |line| {
+                if (eql(u8, line, "")) continue;
+                var cols = splitSca(u8, line, ' ');
+                const col1: u64 = try parseInt(u64, cols.next().?, 10);
+                const col2: u64 = try parseInt(u64, cols.next().?, 10);
+                const col3: u64 = try parseInt(u64, cols.next().?, 10);
+                //try rows.append([3]u64{ col1, col2, col3 });
+                rows.append([3]u64{ col1, col2, col3 }) catch |err| {
+                    print("ERROOOOOOOR: {any}", .{err});
+                };
+            }
+
+            for (rows.items) |row| {
+                if (isSourceMapped(input, row)) {
+                    matched_row = row;
+                    break;
+                }
+            }
+            if (matched_row) |row| {
+                // There is a match
+                if (row[0] > row[1]) {
+                    new_input = (row[0] - row[1]) + input;
+                } else if (row[0] < row[1]) {
+                    new_input = input - (row[1] - row[0]);
+                } else {
+                    // if sourcemap is equal to destmap
+                    new_input = input;
+                }
+            } else {
+                // No match, so result is the same as the input
+                new_input = input;
+            }
+
+            section_count += 1;
+        } // end of section
+
+        sections.reset(); // This is important!
+
+        // At this point, new_input should be equal to the location number,
+        // as it has gone through every section, being mutated in the process.
+        // So we can push it to location_nums.
+        if (new_input) |input| {
+            if (loc_num) |loc| {
+                if (input < loc) {
+                    loc_num = input;
+                }
+            }
+        }
+    } // end of seed number loop
+
+    return loc_num.?;
 }
 
 pub fn main() !void {
--- src/day06.zig
+++ src/day06.zig
@@ -39,9 +39,80 @@ const desc = std.sort.desc;
 
 const data = @embedFile("data/day06.txt");
 
-fn part1() !u32 {}
+fn part1() !usize {
+    var lines = splitSca(u8, data, '\n');
+    const times_str = trim(u8, lines.next().?, "Time:"); // time limits
+    const distances_str = trim(u8, lines.next().?, "Distance:"); // record distances
 
-fn part2() !u32 {}
+    const times_trimmed = std.mem.trimLeft(u8, times_str, " ");
+    const dists_trimmed = std.mem.trimLeft(u8, distances_str, " ");
+
+    print("Times trimmed: {s}\n", .{times_trimmed});
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var times_array: [4]usize = [4]usize{ 0, 0, 0, 0 }; // initialized with zeros
+    var dists_array: [4]usize = [4]usize{ 0, 0, 0, 0 }; // initialized with zeros
+
+    var times_it = splitAny(u8, times_trimmed, " ");
+    var dists_it = splitAny(u8, dists_trimmed, " ");
+
+    // Populate the arrays
+    var i: usize = 0;
+    while (times_it.next()) |time| {
+        if (eql(u8, "", time)) continue;
+        const num = try parseInt(usize, time, 10);
+        times_array[i] = num;
+        i += 1;
+    }
+    i = 0;
+    while (dists_it.next()) |dist| {
+        if (eql(u8, "", dist)) continue;
+        const num = try parseInt(usize, dist, 10);
+        dists_array[i] = num;
+        i += 1;
+    }
+
+    var result: usize = 1;
+    for (times_array, dists_array) |time, record| {
+        // a list might not be needed for part 1, but it might for part 2. Who knows.
+        var best_btn_hold_times: List(usize) = List(usize).init(allocator);
+        var button_time: usize = 1;
+        while (button_time < time) : (button_time += 1) {
+            const time_remaining = time - button_time;
+            const distance: usize = time_remaining * button_time;
+            //print("Distance: {d}\n", .{distance});
+            // if distance is greater than the record, push this to the list
+            if (distance > record) {
+                try best_btn_hold_times.append(button_time);
+            }
+        }
+        result *= best_btn_hold_times.items.len;
+    }
+
+    return result;
+}
+
+fn part2() !u32 {
+    var lines = splitSca(u8, data, '\n');
+    const times_str = trim(u8, lines.next().?, "Time:"); // time limits
+    const distances_str = trim(u8, lines.next().?, "Distance:"); // record distances
+    const first_index_times = indexOfAny(u8, times_str, "1234567890").?;
+    const first_index_dists = indexOfAny(u8, distances_str, "1234567890").?;
+    const time_slice = trim(u8, times_str[first_index_times..], " ");
+    const dists_slice = trim(u8, distances_str[first_index_dists..], " ");
+
+    print("Time line: {s}\n", .{times_str});
+    print("Distance line: {s}\n", .{distances_str});
+    print("first index of times: {d}\n", .{first_index_times});
+    print("first index of dists: {d}\n", .{first_index_dists});
+    print("time slice: {s}\n", .{time_slice});
+    print("dists slice: {s}\n", .{dists_slice});
+
+    return 0;
+}
 
 pub fn main() !void {
     print("Day 06 (Part 1) answer: {any}\n", .{part1()});
