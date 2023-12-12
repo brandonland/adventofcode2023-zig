ccb9c086f25cfebbab620884cf0ff622cc1b63ed
Brandon <brandon@brandon.land>
2023-12-12 04:42:17 -0600

    filtered/organized by hand type

--- src/day07.zig
+++ src/day07.zig
@@ -6,7 +6,7 @@ const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
 
 const util = @import("util.zig");
-const gpa = util.gpa;
+//const gpa = util.gpa;
 
 // Useful stdlib functions
 const eql = std.mem.eql;
@@ -27,6 +27,8 @@ const lastIndexOfStr = std.mem.lastIndexOfLinear;
 const trim = std.mem.trim;
 const sliceMin = std.mem.min;
 const sliceMax = std.mem.max;
+
+const ArrayList = std.ArrayList;
 
 const testing = std.testing;
 
@@ -68,6 +70,7 @@ const Card = enum(u8) {
 };
 
 const HandType = enum(u8) {
+    None = 0,
     HighCard = 1,
     OnePair = 2,
     TwoPair = 3,
@@ -78,14 +81,9 @@ const HandType = enum(u8) {
 
     const Self = @This();
 
-    pub fn strength(self: Self) u8 {
+    pub fn getBaseRank(self: Self) u8 {
         return @intFromEnum(self);
     }
-};
-
-const Duplicate = struct {
-    count: u8,
-    char: u8,
 };
 
 fn isFiveOfAKind(str: []const u8) bool {
@@ -116,18 +114,6 @@ fn getHandType(str: []const u8) HandType {
     if (isFiveOfAKind(str)) return .FiveOfAKind;
     if (isFourOfAKind(str)) return .FourOfAKind;
 
-    //var uniques: [5]u8 = [_]u8{ 0, 0, 0, 0, 0 };
-    //for (uniques[0..], 0..) |_, i| {
-    //    for (str[0..], 0..) |c, j| {
-    //        _ = j;
-
-    //        if (indexOf(u8, &uniques, c) == null) {
-    //            uniques[i] = c;
-    //            break;
-    //        }
-    //    }
-    //}
-
     if (getUniquesCount(str) == 2) {
         return .FullHouse;
     }
@@ -146,7 +132,7 @@ fn getHandType(str: []const u8) HandType {
         return .OnePair;
     }
 
-    // Final fallback: If this code is reached, the only thing it could be is a High Card.
+    // Final fallback: The only other thing it could be.
     return .HighCard;
 }
 
@@ -154,21 +140,30 @@ const Hand = struct {
     cards: [5]Card,
     str: []const u8,
     strength: usize = 0,
-    type: HandType = .FullHouse,
+    type: HandType,
+    rank: u64 = 0,
+    bid: u64 = 1,
 
     const Self = @This();
 
-    pub fn init(self: *Self) void {
-        self.type = determineType(self.str);
-    }
-
-    fn determineType(str: []const u8) HandType {
+    fn getType(str: []const u8) HandType {
         return getHandType(str);
+    }
+    fn getBaseRank(self: *Self) u8 {
+        return self.type.getBaseRank();
+    }
+    pub fn init(self: *Self) void {
+        self.type = getType(self.str);
+        self.rank = getBaseRank(self);
     }
 };
 
 //fn getCardFromChar(char: u8) Card {}
-fn getHandFromStr(str: []const u8) !Hand {
+
+// TODO: This is code smell. If I can instantiate Hands like:
+// `const hand = Hand("AAJAA");`, that would be optimal. May need to return
+// a struct from a `Hand()` function.
+fn createHandFromStr(str: []const u8) !Hand {
     const allocator = std.heap.page_allocator;
 
     const memory = try allocator.alloc(u8, 100);
@@ -191,7 +186,7 @@ fn getHandFromStr(str: []const u8) !Hand {
     try cardmap.put("K", .K);
     try cardmap.put("A", .A);
 
-    var cards: [5]Card = [_]Card{ .A, .A, .A, .A, .A };
+    var cards: [5]Card = [_]Card{ .A, .A, .A, .A, .A }; // Aaaaaaaaaaaa!
     for (str[0..], 0..) |_, i| {
         const end = i + 1;
         const key = str[i..end];
@@ -199,25 +194,126 @@ fn getHandFromStr(str: []const u8) !Hand {
         cards[i] = card.?;
     }
 
-    var hand = Hand{ .cards = cards, .str = str };
+    var hand = Hand{
+        .cards = cards,
+        .str = str,
+        .type = .None,
+    };
     hand.init();
     return hand;
 }
 
-fn part1() !u32 {
-    const card = Card.@"2";
-    print("Card strength: {d}\n", .{card.strength()});
-    print("Card character: {c}\n", .{card.name()});
+const HandList = std.MultiArrayList(Hand);
 
-    var lines = splitSca(u8, data, '\n');
+fn part1() !usize {
+    //const card = Card.@"2";
+    //print("Card strength: {d}\n", .{card.strength()});
+    //print("Card character: {c}\n", .{card.name()});
+    //
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    // Struct of Arrays
+    var soa = HandList{};
+    defer soa.deinit(allocator);
+
+    var num_lines: u32 = 0;
+
+    var lines = tokenizeSca(u8, data, '\n');
     while (lines.next()) |line| {
         var sep = indexOf(u8, line, ' ').?;
-        const hand = line[0..sep];
-        _ = hand;
+
+        const hand_str = line[0..sep];
+        var hand = try createHandFromStr(hand_str);
+        const initial_rank = hand.rank;
+
+        var rank = initial_rank;
+        _ = &rank;
+
         sep += 1;
-        const bid = line[sep..];
-        _ = bid;
+        const bid_str = line[sep..];
+        const bid = try parseInt(usize, bid_str, 10);
+
+        // Attach the bid number to the Hand object
+        hand.bid = bid;
+
+        // TODO: Every Hand needs a unique, newly-generated rank number, which
+        // is initially determined by its base rank (1-7, depending on on HandType).
+        //
+        // This may involve needing to separate every Hand by its base_rank beforehand.
+        // I will have to learn to do this more efficiently. This is probably not
+        // the best approach.
+        //
+        //if ()
+        //
+
+        //switch (hand.type) {
+        //    .HighCard => total_highcards += 1,
+        //    .OnePair => total_onepairs += 1,
+        //    .TwoPair => total_twopairs += 1,
+        //    .ThreeOfAKind => total_threeofkinds += 1,
+        //    .FullHouse => total_fullhouses += 1,
+        //    .FourOfAKind => total_fourofkinds += 1,
+        //    .FiveOfAKind => total_fiveofkinds += 1,
+        //    else => unreachable,
+        //}
+
+        try soa.append(allocator, hand);
+
+        num_lines += 1;
+        print("{d} -- Hand: {s}  |  Bid: {d} -- {s}\n", .{ num_lines, hand_str, hand.bid, @tagName(hand.type) });
     }
+
+    // TODO: Defeat the repetition.
+
+    // Count the number of High Cards.
+    var total_highcards: usize = 0; // 207
+    var total_onepairs: usize = 0; // 211
+    var total_twopairs: usize = 0; // 184
+    var total_threeofkinds: usize = 0; // 187
+    var total_fullhouses: usize = 0; // 110
+    var total_fourofkinds: usize = 0; // 100
+    var total_fiveofkinds: usize = 0; // 1
+
+    print("soa.items.len: {d}\n", .{soa.len});
+
+    for (soa.items(.type)) |t| {
+        if (t == .HighCard) {
+            total_highcards += 1;
+        }
+    }
+    for (soa.items(.type)) |t| {
+        if (t == .OnePair) total_onepairs += 1;
+    }
+    for (soa.items(.type)) |t| {
+        if (t == .TwoPair) total_twopairs += 1;
+    }
+    for (soa.items(.type)) |t| {
+        if (t == .ThreeOfAKind) total_threeofkinds += 1;
+    }
+    for (soa.items(.type)) |t| {
+        if (t == .FullHouse) total_fullhouses += 1;
+    }
+    for (soa.items(.type)) |t| {
+        if (t == .FourOfAKind) total_fourofkinds += 1;
+    }
+    for (soa.items(.type)) |t| {
+        if (t == .FiveOfAKind) total_fiveofkinds += 1;
+    }
+
+    const grand_total = total_highcards + total_onepairs + total_twopairs + total_threeofkinds + total_fullhouses + total_fourofkinds + total_fiveofkinds;
+
+    print("\nTotal high cards: {d}\n", .{total_highcards});
+    print("Total one pairs: {d}\n", .{total_onepairs});
+    print("Total two pairs: {d}\n", .{total_twopairs});
+    print("Total three of a kinds: {d}\n", .{total_threeofkinds});
+    print("Total full houses: {d}\n", .{total_fullhouses});
+    print("Total four of a kinds: {d}\n", .{total_fourofkinds});
+    print("Total five of a kinds: {d}\n", .{total_fiveofkinds});
+    print("Grand total: {d}\n\n", .{grand_total});
+
+    //for (soa.items(.hp)) |*hp| {
+    //    hp.* = 100;
+    //}
 
     return 0;
 }
@@ -231,31 +327,25 @@ pub fn main() !void {
     print("Day 07 (Part 2) answer: {any}\n", .{part2()});
 }
 
-test "Five of a kind" {
-    const hand = try getHandFromStr("QQQQQ");
+test "All Hand types" {
+    const hand = try createHandFromStr("QQQQQ");
     try testing.expectEqual(HandType.FiveOfAKind, hand.type);
-}
-test "Four of a kind" {
-    const hand = try getHandFromStr("KQQQQ");
+
+    hand = try createHandFromStr("KQQQQ");
     try testing.expectEqual(HandType.FourOfAKind, hand.type);
-}
-test "Full House" {
-    const hand = try getHandFromStr("KQQQK");
+
+    hand = try createHandFromStr("KQQQK");
     try testing.expectEqual(HandType.FullHouse, hand.type);
-}
-test "Three of a Kind" {
-    const hand = try getHandFromStr("KQQQJ");
+
+    hand = try createHandFromStr("KQQQJ");
     try testing.expectEqual(HandType.ThreeOfAKind, hand.type);
-}
-test "Two Pair" {
-    const hand = try getHandFromStr("23432");
+
+    hand = try createHandFromStr("23432");
     try testing.expectEqual(HandType.TwoPair, hand.type);
-}
-test "One Pair" {
-    const hand = try getHandFromStr("A23A4");
+
+    hand = try createHandFromStr("A23A4");
     try testing.expectEqual(HandType.OnePair, hand.type);
-}
-test "High Card" {
-    const hand = try getHandFromStr("23456");
+
+    hand = try createHandFromStr("23456");
     try testing.expectEqual(HandType.HighCard, hand.type);
 }
